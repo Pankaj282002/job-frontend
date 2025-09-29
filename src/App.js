61173c623039a7ea7fdfe25be6f43384fcd4ba90
Pankaj282002 <100746779+Pankaj282002@users.noreply.github.com>
2025-09-29 11:11:00 +0530

    remove old navbar

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Footer from './components/Footer';
 function App() {
     return (
         <Router>
-            <AppNavbar />
+            
             <div style={{ minHeight: '80vh' }}>
                 <Routes>
                     <Route path="/" element={<JobDashboard />} />
